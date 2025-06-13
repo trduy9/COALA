35273e9831ab06389f18144a91a6b8a12ed0e8d1
Tduy9 <truongduy2005910@gmail.com>
2025-06-13 13:50:58 +0700

    update

--- cifar10_resnet18.py
+++ cifar10_resnet18.py
@@ -69,7 +69,7 @@ config = {
             "type": "SGD",  
             "lr": 0.01,  
             "momentum": 0.9,  
-            "weight_decay": 0.0001 
+            "weight_decay": 0.0005 
         }  
     },  
     "test_mode": "test_in_server",  
